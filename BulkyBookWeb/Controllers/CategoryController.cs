e6fffb16f40c12c608e5b83d4d5b27624366f3e1
sayan saha <sayan199783@gmail.com>
2023-03-21 11:32:47 +0530

    Initial Commit

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext Db)
+        {
+            _db = Db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> lstCategories = _db.Categories;
+            return View(lstCategories);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            
+            _db.Categories.Add(category);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BulkyBookWeb/Migrations/20230305032343_PopulateCategoriesTable.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Migrations
+{
+    public partial class PopulateCategoriesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO CATEGORIES(NAME,DISPLAYORDER,CREATEDDATETIME) VALUES ('Biography',1,'01/01/2023')");
+            
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM CATEGORIES WHERE NAME = 'Biography'");
+
+        }
+    }
+}
