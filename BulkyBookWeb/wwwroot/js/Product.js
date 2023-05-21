acb6dbacb32df598f57a61a6912ac83796ad38fe
sayan saha <sayan199783@gmail.com>
2023-05-21 18:11:28 +0530

    210523 -Load Datatable

---
+++ BulkyBookWeb/wwwroot/js/Product.js
@@ -0,0 +1,14 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+});
+function loadDataTable() {
+    dataTable = $('#tblProduct').DataTable({
+        "ajax": { "url": "/Admin/Product/GetAll" },
+        "columns": [{ "data": "title", "width": "15%" },
+            { "data": "author", "width": "15%" },
+            { "data": "isbn", "width": "15%" },
+            { "data": "price", "width": "15%" }
+        ]
+    });
+}
